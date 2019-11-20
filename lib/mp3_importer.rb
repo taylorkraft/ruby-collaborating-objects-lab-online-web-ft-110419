ace936c35f828d283c8bec51b5ef8b0ff0315c0f
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-20 15:27:38 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+class MP3Importer
+  attr_accessor :path 
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files
+    Dir.entries(path).select {|entry| entry.include?("mp3")}
+  end
+  
+  def import 
+    self.files.each {|filename| Song.new_by_filename(filename)}
+  end 
+end
+
